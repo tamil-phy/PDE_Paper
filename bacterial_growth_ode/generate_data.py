f4f4045cee7a14502da6b99f36da276927b7de07
vanangamudi <selva.developer@gmail.com>
2021-05-27 21:14:55 +0530

    bacterial growth modelling using ode 1d

---
+++ bacterial_growth_ode/generate_data.py
@@ -0,0 +1,50 @@
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Set the carrying capacity:
+K = [75, 1.5E2, 3E2]
+
+# Extend the total time of the integration
+delta_t = 0.1
+N_0 = 1
+r = 0.03
+total_time = 500
+time_range = np.arange(0, total_time, delta_t)
+n_time_steps = int(total_time / delta_t)
+# Set the storage vector so we don't rewrite our correct approach
+N_t = np.zeros((len(K), n_time_steps))
+N_t[:,0] = N_0
+
+# Loop through each carrying capacity
+for k in range(len(K)): 
+    
+    # Loop through each time step.
+    for t in range(1, n_time_steps): 
+    
+        # Calculate the change in the number of cells. 
+        dN = N_t[k, t - 1] * r * delta_t * (1 - N_t[k, t - 1] / K[k])
+    
+        # Update the number of cells at the current time point
+        N_t[k, t] = N_t[k, t - 1] + dN
+
+
+N_t = N_t.transpose()
+
+time_range = time_range.reshape(5000, 1) #TODO: fix this hardcoding
+with open('data.pkl', 'wb') as f:
+    print('dumping data...')
+    print('shapes: {}, {}'.format(time_range.shape, N_t.shape))
+    pickle.dump((time_range, N_t), f)
+    
+# Loop through the carrying capacities and plot every 100th point.
+for i in range(len(K)):
+    plt.plot(time_range[::100, 0], N_t[::100, i], '.', label='K = ' + str(K[i]))
+
+
+    
+# Add appropriate labels and legends. 
+plt.xlabel('time  [min]')
+plt.ylabel('number of cells')
+plt.legend() 
+plt.show()
---
+++ bacterial_growth_ode/model.py
@@ -0,0 +1,101 @@
+import os
+import copy
+import sys
+import pdb
+import shutil
+import pickle
+import json
+import random
+
+from collections import namedtuple, defaultdict, Counter
+from pprint import pprint
+from tqdm import tqdm
+
+import matplotlib.pyplot as plt
+
+import torch
+from torch import nn
+from torch.utils.data import DataLoader, Dataset
+import model_base
+
+import CONFIG
+
+def plot_results_XY(trainer, epoch):
+    input_, target, output = trainer.eval_epoch(epoch)
+    input_, target, output = [i.detach().cpu() for i in [input_, target, output]]
+    plt.scatter(input_.cpu(), target.cpu(), label='x')
+    plt.scatter(input_.cpu(), output.cpu(), label='x\'')
+    plt.legend()
+    plt.show()
+
+
+def plot_results_TS(trainer, epoch):
+    input_, target, output = trainer.eval_epoch(epoch)
+    print('shapes: input_, target, ouptut: {}, {}, {}'.format(input_.size(),
+                                                              target.size(),
+                                                              output.size()))
+    
+    input_, target, output = [i.detach().cpu() for i in [input_, target, output]]
+
+    plt.plot(range(target.size(0)), target[:, 1].cpu(), label='x')
+    plt.scatter(range(target.size(0)), output[:, 1].cpu(), label='x\'')
+    plt.plot(range(target.size(0)), target[:, 2].cpu(), label='y')
+    plt.scatter(range(target.size(0)), output[:, 2].cpu(), label='y\'')
+
+    plt.legend()
+    plt.show()
+    
+    plt.plot(target[:, 1], target[:, 2],  label='x')
+    plt.plot(output[:, 1], output[:, 2], '--', label='y')
+
+    
+    plt.plot(target[0, 1], target[0, 2], 'b>', label='x start')
+    plt.plot(target[-1, 1], target[-1, 2], 'r>', label='x end')
+
+    plt.plot(output[0, 1], output[0, 2], 'bo' , label='x\' start')
+    plt.plot(output[-1, 1], output[-1, 2], 'ro', label='x\' end')
+
+    plt.legend()
+    plt.show()
+
+
+
+if __name__ == '__main__':
+
+    filepath = 'data.pkl'
+    seq_length = 19
+    ts, vals = pickle.load(open(filepath, 'rb'))
+    dataset = model_base.TSDataset(ts, vals, seq_length)
+
+    random_sample  = random.choice(dataset)
+    print('random sample: ', random_sample)
+    input_, output = random_sample
+    model = model_base.TSModel(input_.size()[-1],
+                               output.size()[-1],
+                               100,
+                               1,
+                               seq_length)
+
+    weights_path = filepath.replace('.pkl', '.pt')
+    if os.path.exists(weights_path):
+        print('loading old model....')
+        model.load_state_dict(torch.load(weights_path))
+    else:
+        model.apply(model_base.weights_init_uniform)
+        pass
+    
+    trainer = model_base.Trainer (
+        model,
+        torch.nn.L1Loss(),
+        torch.optim.Adam(model.parameters()),
+
+        dataset,        
+        dataset,
+        batch_size = 10000,
+
+        weights_path = weights_path
+    )
+
+    trainer.do_train()
+
+    plot_results(trainer, epoch)
